46937962278129c7e502c59a830a29e14e691e92
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-02-26 22:21:05 +0000

    script

--- script.py
+++ script.py
@@ -12,11 +12,12 @@ if not check:
     raise Exception
 
 model = models.CellposeModel(model_type="bact_phase_cp3", gpu=True)
+model_diam_mean = models.CellposeModel(model_type="bact_phase_cp3", gpu=True, diam_mean=20)
 gnome = Path("/users/ach22jc/test.tif")
 
 # paramaters to play with later
-anisotropy = 488 / 19
-diameter = 20  # used to resample the image? wild try it out
+# anisotropy = 488 / 19 # this works bad
+# diameter = 20  # used to resample the image? wild try it out
 
 tif = imread(gnome)
 
@@ -26,36 +27,19 @@ tif = np.moveaxis(tif, 1, -1)
 tif = np.max(tif, axis=3)
 print(tif.shape)
 
+for i in range(10):
+    flow = 2 * i
+    mask, two, three = model.eval(
+        tif, do_3D=True, z_axis=0, flow3D_smooth=flow
+    )
+    print(str(i) + " done")
 
-# for i in range(10):
-    # flow = 4 * i
+    outstr = "/users/ach22jc/test-outputs/out-standard" + str(i) + ".tif"
+    imwrite(outstr, mask)
 
-mask, two, three = model.eval(
-    tif, do_3D=True, z_axis=0, flow3D_smooth=10
-)
-outstr = "/users/ach22jc/test-outputs/out-none.tif"
-imwrite(outstr, mask)
-
-mask, two, three = model.eval(
-    tif, do_3D=True, z_axis=0, diameter=diameter, flow3D_smooth=10
-)
-outstr = "/users/ach22jc/test-outputs/out-dia.tif"
-imwrite(outstr, mask)
-
-mask, two, three = model.eval(
-    tif, do_3D=True, z_axis=0, anisotropy=anisotropy, flow3D_smooth=10
-)
-outstr = "/users/ach22jc/test-outputs/out-ani.tif"
-imwrite(outstr, mask)
-
-mask, two, three = model.eval(
-    tif, do_3D=True, z_axis=0, diameter=diameter, anisotropy=anisotropy, flow3D_smooth=10
-)
-outstr = "/users/ach22jc/test-outputs/out-all.tif"
-imwrite(outstr, mask)
-
-    # print(str(i) + " done")
-    #
-    # print(mask.shape)
-    # outstr = "/users/ach22jc/test-outputs/out" + str(i) + ".tif"
-    # imwrite(outstr, mask)
+    mask, two, three = model_diam_mean.eval(
+        tif, do_3D=True, z_axis=0, flow3D_smooth=flow
+    )
+    outstr = "/users/ach22jc/test-outputs/out-diam-mean" + str(i) + ".tif"
+    imwrite(outstr, mask)
+    print(str(i) + " done")
