7a508c99ed2ce10f70aaa00f4a64833921e9ab27
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-02-26 21:41:07 +0000

    script

--- script.py
+++ script.py
@@ -19,13 +19,35 @@ tif = np.max(tif, axis=3)
 print(tif.shape)
 
 
-for i in range(10):
-    flow = 4 * i
-    mask, two, three = model.eval(
-        tif, do_3D=True, z_axis=0, diameter=diameter, anisotropy=anisotropy, flow3D_smooth=flow
-    )
-    print(str(i) + " done")
+# for i in range(10):
+    # flow = 4 * i
 
-    print(mask.shape)
-    outstr = "/users/ach22jc/test-outputs/out" + str(i) + ".tif"
-    imwrite(outstr, mask)
+mask, two, three = model.eval(
+    tif, do_3D=True, z_axis=0, flow3D_smooth=10
+)
+outstr = "/users/ach22jc/test-outputs/out-none.tif"
+imwrite(outstr, mask)
+
+mask, two, three = model.eval(
+    tif, do_3D=True, z_axis=0, diameter=diameter, flow3D_smooth=10
+)
+outstr = "/users/ach22jc/test-outputs/out-dia.tif"
+imwrite(outstr, mask)
+
+mask, two, three = model.eval(
+    tif, do_3D=True, z_axis=0, anisotropy=anisotropy, flow3D_smooth=10
+)
+outstr = "/users/ach22jc/test-outputs/out-ani.tif"
+imwrite(outstr, mask)
+
+mask, two, three = model.eval(
+    tif, do_3D=True, z_axis=0, diameter=diameter, anisotropy=anisotropy, flow3D_smooth=10
+)
+outstr = "/users/ach22jc/test-outputs/out-all.tif"
+imwrite(outstr, mask)
+
+    # print(str(i) + " done")
+    #
+    # print(mask.shape)
+    # outstr = "/users/ach22jc/test-outputs/out" + str(i) + ".tif"
+    # imwrite(outstr, mask)
