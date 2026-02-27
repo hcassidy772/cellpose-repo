11cc3514b891670bae861644c785cff3af35a91a
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-02-27 13:59:51 +0000

    script

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ if not check:
 
 # dev = torch.cuda.device(0)
 
-model = models.CellposeModel(model_type="bact_phase_cp3", gpu=True, device=dev)
+model = models.CellposeModel(model_type="bact_phase_cp3", gpu=True)
 gnome = Path("/users/ach22jc/test.tif")
 
 # paramaters to play with later
