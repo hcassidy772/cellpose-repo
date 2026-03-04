67b51905f9e6b1d3aebcb1546713e70a7dbe4f7d
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-03-04 09:17:23 +0000

    script4

--- script4.py
+++ script4.py
@@ -1,6 +1,6 @@
 from cellpose import models
 from pathlib import Path
-# import numpy as np
+import numpy as np
 from tifffile import imread, imwrite
 import torch
 
@@ -14,9 +14,9 @@ if not check:
 # 4.0
 model = models.CellposeModel(gpu=True)
 
-gnome = Path("/users/ach22jc/v2/v2/")
+gnome = Path("/users/ach22jc/test-images/")
+tifs = gnome.glob('*.tif')
 
-tif = imread(gnome)
 
 # base values
 # diameter = 20
@@ -25,86 +25,20 @@ tif = imread(gnome)
 # flow_threshold = 0.8
 flow3D_smooth = 2
 
-print(tif.shape)
 
 print('running diam')
-for i in range(1, 21):  # dont run diam 0 you spanner
-    diam = 2 * i
+for i in tifs:
+    tif = imread(i)
+    print(tif.shape)
+    if i.ndim == 4:
+        tif = np.max(tif, axis=1)
     mask, two, three = model.eval(
         tif,
         do_3D=True,
         z_axis=0,
-        diameter=diam,
-        # cellprob_threshold=cellprob_threshold,
-        # flow_threshold=flow_threshold,
-        # min_size=min_size,
-        # flow3D_smooth=flow3D_smooth
+        flow3D_smooth=flow3D_smooth
     )
-    outstr = "/users/ach22jc/test-outputs/cp4/diam/" + str(i) + ".tif"
+    outstr = "/users/ach22jc/test-outputs/cp4/trial/" + (i.name)
     imwrite(outstr, mask)
 
-print('running cellprob_threshold')
-for i in range(11):
-    cpt = i / 10
-    mask, two, three = model.eval(
-        tif,
-        do_3D=True,
-        z_axis=0,
-        # diameter=diameter,
-        cellprob_threshold=cpt,
-        # flow_threshold=flow_threshold,
-        # min_size=min_size,
-        # flow3D_smooth=flow3D_smooth
-    )
-    outstr = "/users/ach22jc/test-outputs/cp4/cpt/" + str(i) + ".tif"
-    imwrite(outstr, mask)
-
-print('running flow_threshold')
-for i in range(11):
-    ft = i / 10
-    mask, two, three = model.eval(
-        tif,
-        do_3D=True,
-        z_axis=0,
-        # diameter=diameter,
-        # cellprob_threshold=cellprob_threshold,
-        flow_threshold=ft,
-        # min_size=min_size,
-        # flow3D_smooth=flow3D_smooth
-    )
-    outstr = "/users/ach22jc/test-outputs/cp4/ft/" + str(i) + ".tif"
-    imwrite(outstr, mask)
-
-print('running min_size')
-for i in range(1, 6):
-    ms = 25 * i
-    mask, two, three = model.eval(
-        tif,
-        do_3D=True,
-        z_axis=0,
-        # diameter=diameter,
-        # cellprob_threshold=cellprob_threshold,
-        # flow_threshold=flow_threshold,
-        min_size=ms,
-        # flow3D_smooth=flow3D_smooth
-    )
-    outstr = "/users/ach22jc/test-outputs/cp4/ms/" + str(i) + ".tif"
-    imwrite(outstr, mask)
-
-
-# print('running flow3D_smooth')
-# for i in range(10):
-#     f3d = 2 * i
-#     mask, two, three = model.eval(
-#         tif,
-#         do_3D=True,
-#         z_axis=0,
-#         # diameter=diameter,
-#         # cellprob_threshold=cellprob_threshold,
-#         # flow_threshold=flow_threshold,
-#         # min_size=min_size,
-#         flow3D_smooth=f3d
-#     )
-#     outstr = "/users/ach22jc/test-outputs/cp4/f3d/" + str(i) + ".tif"
-#     imwrite(outstr, mask)
 print('tada')
