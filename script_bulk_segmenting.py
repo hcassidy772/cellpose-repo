e12214d4f3d08c8972b2479282b04239920e0348
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-03-05 14:20:44 +0000

    restructure

--- script_bulk_segmenting.py
+++ script_bulk_segmenting.py
@@ -7,21 +7,22 @@ import logging
 
 check = torch.cuda.is_available()
 
-logging.basicConfig(filename='4.log', level=logging.INFO)
+logging.basicConfig(filename="v2.log", level=logging.INFO)
 
 if not check:
     print("gpu not available")
     print("ending")
     raise Exception
 
+# ========== cellpose setup ==========
+
 # 4.0
 model = models.CellposeModel(gpu=True)
 
-# gnome = Path("/users/ach22jc/test-images/")
-# tifs = gnome.glob("*.tif")
+# gnome = Path("/users/ach22jc/hnt.tif/")
 
 # tifs = list(Path('/users/ach22jc/atto/').glob('*.tif'))
-tifs = list(Path('/users/ach22jc/hnt/').glob('*.tif'))
+tifs = list(Path("/users/ach22jc/hnt/").glob("*.tif"))
 # rf470 = tifs + list(Path('/users/ach22jc/rf470/').glob('*.tif'))
 # shh = tifs + list(Path('/users/ach22jc/shh/').glob('*.tif'))
 # shl = tifs + list(Path('/users/ach22jc/shl/').glob('*.tif'))
@@ -33,17 +34,18 @@ tifs = list(Path('/users/ach22jc/hnt/').glob('*.tif'))
 flow_threshold = 0.1
 flow3D_smooth = 2
 
-
+# ========== for loop ==========
 for i in tifs:
     tif = imread(i)
-    print(i.name)
-    print(tif.shape)
+
     if tif.ndim == 4:
         tif = np.max(tif, axis=1)
+
     mask, two, three = model.eval(
-        tif, do_3D=True, z_axis=0, flow3D_smooth=flow3D_smooth
+        tif, do_3D=True, z_axis=0, flow3D_smooth=flow3D_smooth, flow_threshold=(i / 10)
     )
-    outstr = "/users/ach22jc/test-outputs/cp4/bulk/" + (i.name)
+    # outstr = "/users/ach22jc/test-outputs/cp4/v2/" + (i.name[27:29]) + '-cellposed' + ".tif"
+    outstr = "/users/ach22jc/test-outputs/cp4/hnt/ft/" + i.name
     imwrite(outstr, mask)
 
 print("tada")
---
+++ script_param_testing.py
@@ -0,0 +1,56 @@
+from cellpose import models
+from pathlib import Path
+import numpy as np
+from tifffile import imread, imwrite
+import torch
+import logging
+
+check = torch.cuda.is_available()
+
+logging.basicConfig(filename="4.log", level=logging.INFO)
+
+if not check:
+    print("gpu not available")
+    print("ending")
+    raise Exception
+
+# ========== cellpose setup ==========
+
+# 4.0
+model = models.CellposeModel(gpu=True)
+
+# gnome = Path("/users/ach22jc/test-images/")
+gnome = Path("/users/ach22jc/hnt.tif")
+tif = imread(gnome)
+# tifs = gnome.glob("*.tif")
+
+# base values
+# diameter = 20
+# min_size = 12
+# cellprob_threshold = 5
+flow_threshold = 0.1
+flow3D_smooth = 2
+
+# ========== for loop ==========
+
+for i in range(11):
+    if tif.ndim == 4:
+        tif = np.max(tif, axis=1)
+    mask, two, three = model.eval(
+        tif, do_3D=True, z_axis=0, flow3D_smooth=flow3D_smooth,
+        flow_threshold=(i / 10)
+    )
+    outstr = "/users/ach22jc/test-outputs/cp4/hnt/ft/" + (i) + '.tif'
+    imwrite(outstr, mask)
+
+
+# for i in range():
+#     if tif.ndim == 4:
+#         tif = np.max(tif, axis=1)
+#     mask, two, three = model.eval(
+#         tif, do_3D=True, z_axis=0, flow3D_smooth=flow3D_smooth
+#     )
+#     outstr = "/users/ach22jc/test-outputs/cp4/bulk/" + (i.name)
+#     imwrite(outstr, mask)
+
+print("tada")
