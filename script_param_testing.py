820371af33a8b95f8826f8de8613885d1c8147a1
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-03-10 19:14:45 +0000

    run prep

--- script_param_testing.py
+++ script_param_testing.py
@@ -1,6 +1,6 @@
 from cellpose import models
-from pathlib import Path
-import numpy as np
+# from pathlib import Path
+# import numpy as np
 from tifffile import imread, imwrite
 import torch
 import logging
@@ -19,9 +19,15 @@ if not check:
 # 4.0
 model = models.CellposeModel(gpu=True)
 
+tifs = []
 # gnome = Path("/users/ach22jc/test-images/")
-gnome = Path("/users/ach22jc/rf470.tif")
-tif = imread(gnome)
+# gnome = Path("/users/ach22jc/rf470.tif")
+# tif = imread(gnome)
+tifs.append(imread('/users/ach22jc/atto-l.tif')) # 25
+tifs.append(imread('/users/ach22jc/atto-s.tif')) # 15
+tifs.append(imread('/users/ach22jc/shh.tif')) # 25-30
+tifs.append(imread('/users/ach22jc/shh.tif')) # 25-30
+# shh = imread('/users/ach22jc/shl.tif')
 # tifs = gnome.glob("*.tif")
 
 # base values
@@ -30,13 +36,13 @@ tif = imread(gnome)
 # cellprob_threshold = 5
 # flow_threshold = 0.1  # doesnt work for 3D
 flow3D_smooth = 2  # :thumb:
-min_diam = 18  # not very useful, good for removing bg noise on larger images but doesnt affect mask quality
+min_diam = 10  # not very useful, good for removing bg noise on larger images but doesnt affect mask quality
 
 # ========== other setup ==========
 
 
 def vol(diam):
-    return (4 / 3) * (3.14) * ((diam / 2) ** 3)  # eq for sphere vol
+    return int((4 / 3) * (3.14) * ((diam / 2) ** 3))  # eq for sphere vol
 
 
 # ========== for loop ==========
@@ -52,14 +58,15 @@ def vol(diam):
 #     imwrite(outstr, mask)
 
 
-for i in range(10):
+for i in tifs:
     # if tif.ndim == 4:
     #     tif = np.max(tif, axis=1)
     mask, two, three = model.eval(
-        tif, do_3D=True, z_axis=0,
-        flow3D_smooth=i
+        i, do_3D=True, z_axis=0,
+        flow3D_smooth=4,
+        min_diam=vol(min_diam)
     )
-    outstr = "/users/ach22jc/test-outputs/cp4/rf470/f3d/" + str(i) + ".tif"
+    outstr = "/users/ach22jc/test-outputs/cp4/param/" + str(i) + ".tif"
     imwrite(outstr, mask)
 
 print("tada")
