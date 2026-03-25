ff2cc36e4d2e5ef57e0665b1b4d2052e2f231c2e
hannah cassidy <hccassidy1@sheffield.ac.uk>
2026-03-25 13:37:01 +0000

    opt run model 6

--- opt.py
+++ opt.py
@@ -5,7 +5,7 @@ from csbdeep.utils import normalize
 
 from util import merge
 
-model = StarDist3D(None, name='model-5-s', basedir='/users/ach22jc/models/')
+model = StarDist3D(None, name='model-6-s', basedir='/users/ach22jc/models/')
 
 raw_dir = "/mnt/parscratch/users/ach22jc/tifs/sdist-raw/"
 mas_dir = "/mnt/parscratch/users/ach22jc/tifs/sdist-mas/"
